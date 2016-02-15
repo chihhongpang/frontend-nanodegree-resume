c0c4a9998d619c06d22b2750d20951cf938e8e1c
Chih-Hong Pang <chihhong.pang@gmail.com>
2016-02-15 10:44:43 -0500

    Add bio.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,25 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Chih-Hong James Pang");
+var name = "Chih-Hong James Pang";
+var role = "Software Developer";
+$("#main").append(name);
+
+var email = "example@gmail.com";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var picture = "images/fry.jpg";
+var welcomeMsg = "How are you doing today?";
+var skills = ["Java", "Team Work"];
+var bio = { "name": name,
+			"role" : role,
+			"email" : email,
+			"picture" : picture,
+			"welcomeMsg": welcomeMsg,
+			"skills" : skills
+};
+$("#main").prepend(bio.welcomeMsg);
